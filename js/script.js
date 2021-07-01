d67dbaa293f38018c4ffdcac39aaacf5d34eba69
amaterasu0912 <amaterasu0912@mail.ru>
2021-07-01 18:02:19 +0500

    second task

--- js/script.js
+++ js/script.js
@@ -8,15 +8,27 @@ let personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?'),
-    b = prompt('На сколько его оцените?'),
-    c = prompt('Один из последних просмотренных фильмов?'),
-    d = prompt('На сколько его оцените?');
 
-let movies = {
-    a:b,
-    c:d
-};
+for (let i = 0; i < 2; i++){
+    let a = prompt('Один из последних просмотренных фильмов?'),
+        b = prompt('На сколько его оцените?');
+        if (a != null && b != null && a != ''  && b != '' && a.length <= 50 && b.length <= 50){
+            personalMovieDB.movies[a] = b; 
+        } else {
+            a = prompt('Один из последних просмотренных фильмов?');
+            b = prompt('На сколько его оцените?');
+            personalMovieDB.movies[a] = b; 
+    }  
+}
 
-console.log(movies);
+if (personalMovieDB.count < 10 ){
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
 console.log(personalMovieDB);
