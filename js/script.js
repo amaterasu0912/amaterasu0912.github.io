f8083bffb3df6780c73c06001d6b85d84e3f39d0
amaterasu0912 <amaterasu0912@mail.ru>
2021-09-27 17:13:42 +0500

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-
+var swiper = new Swiper(".ourProductions", {
+    slidesPerView: "auto",
+    spaceBetween: 0,
+    loop: true,
+  });
     
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    grid: {
+      rows: 2,
+    },
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
